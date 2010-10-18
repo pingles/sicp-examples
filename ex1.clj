31472132951eef6d349a0a17f39011431f5d7657
Paul Ingles <paul@oobaloo.co.uk>
2010-10-18 21:16:46 +0100

    recursive process implementation of example 1.11

--- ex1.clj
+++ ex1.clj
@@ -253,3 +253,20 @@
 ;;2^2 for n = 2
 ;;((2^2)^2) for n = 3
 ;;((((2^2)^2)^2)^2) for n = 4
+
+
+;;Example 1.11
+;;A function f is defined by the rule that f(n) = n if n<3 and
+;;f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n> 3. Write a procedure
+;;that computes f by means of a recursive process.
+;;Write a procedure that computes f by means of an iterative process.
+
+(defn mystery-fn
+  [n]
+  (if (< n 3)
+    n
+    (+ (mystery-fn (- n 1))
+       (* 2
+          (mystery-fn (- n 2)))
+       (* 3
+          (mystery-fn (- n 3))))))
