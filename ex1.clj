2d0d1fde0988275d1b6dfab3118cfd1408261404
Paul Ingles <paul@oobaloo.co.uk>
2010-10-31 16:04:28 +0000

    Add code for exercise 1.16

--- ex1.clj
+++ ex1.clj
@@ -433,4 +433,25 @@
                  (fast-expt b
                             (dec n)))))
 
+;; Exercise 1.16.  Design a procedure that evolves an iterative
+;; exponentiation process that uses successive squaring and uses a
+;; logarithmic number of steps, as does fast-expt. (Hint: Using the
+;; observation that (b^n/2)^2 = (b^2)^n/2, keep, along with the exponent n
+;; and the base b, an additional state variable a, and define the
+;; state transformation in such a way that the product a bn is
+;; unchanged from state to state. At the beginning of the process a is
+;; taken to be 1, and the answer is given by the value of a at the end
+;; of the process. In general, the technique of defining an invariant
+;; quantity that remains unchanged from state to state is a powerful
+;; way to think about the design of iterative algorithms.)
 
+(defn fast-expt-iter
+  ([b n] (fast-expt-iter b n 1))
+  ([b n a]
+     (cond (= n 0) a
+           (even? n) (recur (square b)
+                            (/ n 2)
+                            a)
+           :else (recur b
+                        (dec n)
+                        (* a b)))))
